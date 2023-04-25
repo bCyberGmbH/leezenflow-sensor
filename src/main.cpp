6631b3dbcc2bf74d393604170c1be42aa6cc7129
Gerald Pape <mail@geraldpape.io>
2023-04-25 15:42:05 +0200

    Change output format into single line + use mV for Voltage

--- src/main.cpp
+++ src/main.cpp
@@ -35,18 +35,18 @@ void loop()
     tmp = 0, hum = 0;
   }
 
-  voltmeter.getValue();
+  voltage_value = voltmeter.getValue();
   voltage_value = voltmeter.adc_raw * voltmeter.resolution * voltmeter.calibration_factor;
   // normalize to positive voltage
   if (voltage_value < 0) voltage_value = voltage_value * -1;
-  voltage_value = voltage_value / 1000.0;
 
   Serial.printf(
       // C = temperatur in Celsius
       // H = humidity in percent
       // P = air pressure in Pa
-      // V = battery current in V
-      "C:%2.1f\r\nH:%2.0f%\r\nP:%2.0f\r\nV:%1.2f\r\n", tmp,
-      hum, pressure, voltage_value);
+      // V = battery current in mV
+      // C,H,P,V
+      "%2.1f,%2.0f,%2.0f,%d\r\n",
+      tmp, hum, pressure, voltage_value);
   delay(2000);
 }
